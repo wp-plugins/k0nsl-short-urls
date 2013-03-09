2a7b33391f82064bbf71f0ba5ae02407dabc18ff
k0nsl <k0nsl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-09 02:41:33 +0000

    Initial commit.
    git-svn-id: https://plugins.svn.wordpress.org/k0nsl-short-urls/trunk@678527 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ k0nsl_shorturl.php
@@ -0,0 +1,164 @@
+<?php
+/*
+Plugin Name: k0nsl Short URLs
+Plugin URI: http://k0nsl.org/blog/k0nsl-short-urls-plugin/
+Description: Automatically shortens the blog post URL.
+Version: 0.1
+Author: GentleSource (and k0nsl)
+Author URI: http://k0nsl.org/blog/
+*/
+
+define('DEFAULT_API_URL', 'http://knsl.net/api.php?url=%s');
+define( 'k0nsl_plugin_path', plugin_dir_path(__FILE__) );
+
+class k0nsl_Short_URL
+{
+    /**
+     * List of short URL website API URLs (only knsl.net for now)
+     */
+    function api_urls()
+    {
+        return array(
+            array(
+                'name' => 'knsl.net',
+                'url'  => 'http://knsl.net/api.php?url=%s',
+                ),
+            );
+    }
+
+    /**
+     * Create short URL based on post URL
+     */
+    function create($post_id)
+    {
+        if (!$apiURL = get_option('k0nslShortUrlApiUrl')) {
+            $apiURL = DEFAULT_API_URL;
+        }
+
+        // For some reason the post_name changes to /{id}-autosave/ when a post is autosaved
+        $post = get_post($post_id);
+        $pos = strpos($post->post_name, 'autosave');
+        if ($pos !== false) {
+            return false;
+        }
+        $pos = strpos($post->post_name, 'revision');
+        if ($pos !== false) {
+            return false;
+        }
+
+        $apiURL = str_replace('%s', urlencode(get_permalink($post_id)), $apiURL);
+
+        $result = false;
+
+        if (ini_get('allow_url_fopen')) {
+            if ($handle = @fopen($apiURL, 'r')) {
+                $result = fread($handle, 4096);
+                fclose($handle);
+            }
+        } elseif (function_exists('curl_init')) {
+            $ch = curl_init($apiURL);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+            $result = @curl_exec($ch);
+            curl_close($ch);
+        }
+
+        if ($result !== false) {
+            delete_post_meta($post_id, 'knslShortURL');
+            $res = add_post_meta($post_id, 'knslShortURL', $result, true);
+            return true;
+        }
+    }
+
+    /**
+     * Option list (default settings)
+     */
+    function options()
+    {
+        return array(
+           'ApiUrl'         => DEFAULT_API_URL,
+           'Display'        => 'Y',
+           'TwitterLink'    => 'Y',
+           );
+    }
+
+    /**
+     * Plugin settings
+     *
+     */
+    function settings()
+    {
+        $apiUrls = $this->api_urls();
+        $options = $this->options();
+        $opt = array();
+
+        if (!empty($_POST)) {
+            foreach ($options AS $key => $val)
+            {
+                if (!isset($_POST[$key])) {
+                    continue;
+                }
+                update_option('knslShortURL' . $key, $_POST[$key]);
+            }
+        }
+        foreach ($options AS $key => $val)
+        {
+            $opt[$key] = get_option('knslShortURL' . $key);
+        }
+        include k0nsl_plugin_path . 'template/settings.tpl.php';
+    }
+
+    /**
+     *
+     */
+    function admin_menu()
+    {
+        add_options_page('k0nsl Short URL', 'Short URLs', 10, 'k0nsl_shorturl-settings', array(&$this, 'settings'));
+    }
+
+    /**
+     * Display the short URL
+     */
+    function display($content)
+    {
+
+        global $post;
+
+        if ($post->ID <= 0) {
+            return $content;
+        }
+
+        $options = $this->options();
+
+        foreach ($options AS $key => $val)
+        {
+            $opt[$key] = get_option('knslShortURL' . $key);
+        }
+
+        $shortUrl = get_post_meta($post->ID, 'knslShortURL', true);
+
+        if (empty($shortUrl)) {
+            return $content;
+        }
+
+        $shortUrlEncoded = urlencode($shortUrl);
+
+        ob_start();
+        include k0nsl_plugin_path . 'template/public.tpl.php';
+        $content .= ob_get_contents();
+        ob_end_clean();
+
+        return $content;
+    }
+}
+
+$gssu = new k0nsl_Short_URL;
+
+if (is_admin()) {
+    add_action('edit_post', array(&$gssu, 'create'));
+    add_action('save_post', array(&$gssu, 'create'));
+    add_action('publish_post', array(&$gssu, 'create'));
+    add_action('admin_menu', array(&$gssu, 'admin_menu'));
+} else {
+    add_filter('the_content', array(&$gssu, 'display'));
+}
---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== k0nsl Short URLs ===
+Contributors: GentleSource (and k0nsl)
+Tags: short url, short, url, shortener, url shortener, url shortening, urls, links, tinyurl, twitter, microblogging, k0nsl
+Requires at least: 2.5
+Tested up to: 3.6
+
+Automatically shortens the blog post URL via knsl.net
+
+== Description ==
+
+This plugin creates a short URL from the blog post permalink and stores it
+in the database. The URL is displayed below the blog post along with a link
+to twitter that passes the short URL on.
+The plugin currently use knsl.net as the only service and stores the data in the database of knsl.net
+
+== Installation ==
+
+1. Upload the `k0nsl_shorturl` folder to `/wp-content/plugins/`.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. You can change the default short URL service in 'Settings' -> 'Short URLs'
+
+== Changelog ==
+
+= 0.1 =
+
+* Replaced hardcoded plugin directory path with a defined path via "k0nsl_plugin_path".
+
+== Frequently Asked Questions ==
+
+No questions yet.
+
+== Screenshots ==
+
+1. Short URL settings page
